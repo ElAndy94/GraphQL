2ce57527fdf7cffec07005286be653f5105b4d72
Andrew <andrewpeliza@hotmail.com>
2019-02-06 16:42:51 +0000

    Can login/create user via the frontend using Graphql.

--- frontend/src/containers/Auth.js
+++ frontend/src/containers/Auth.js
@@ -1,23 +1,87 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './Auth.css';
+import "./Auth.css";
 
 class Auth extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogin: true
+    };
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
+  }
+
+  switchModeHandler = () => {
+    this.setState(prevState => {
+      return { isLogin: !prevState.isLogin };
+    });
+  };
+
+  submitHandler = event => {
+    event.preventDefault();
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `
+    };
+
+    if (!this.state.isLogin) {
+      requestBody = {
+        query: `
+          mutation {
+            createUser(userInput: {email: "${email}", password: "${password}"}) {
+              _id
+              email
+            }
+          }
+        `
+      };
+    }
+
+    axios
+      .post("http://localhost:8000/graphql", requestBody)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
-    return <form className="auth-form">
-      <div className="form-control">
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
-      </div>
-      <div className="form-control">
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
-      </div>
-      <div className="form-actions">
-        <button type="submit">Submit</button>
-        <button type="button">Switch to Signup</button>
-      </div>
-    </form>
+    return (
+      <form className="auth-form" onSubmit={this.submitHandler}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" ref={this.emailEl} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={this.passwordEl} />
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button" onClick={this.switchModeHandler}>
+            Switch to {this.state.isLogin ? "Signup" : "Login"}
+          </button>
+        </div>
+      </form>
+    );
   }
 }
 
