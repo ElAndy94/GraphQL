c9c64930c54d4b4c28e7a11c463e68ca684fd300
Andrew <andrewpeliza@hotmail.com>
2019-02-07 18:15:59 +0000

    Bookings can be deleted now.

--- frontend/src/containers/Booking/Booking.js
+++ frontend/src/containers/Booking/Booking.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-import AuthContext from '../../context/auth-context';
-import Spinner from '../../components/Spinner/Spinner';
+import AuthContext from "../../context/auth-context";
+import Spinner from "../../components/Spinner/Spinner";
+import BookingList from "../../components/Bookings/BookingList/BookingList";
 
 class Booking extends Component {
   state = {
     isLoading: false,
     bookings: []
-  }
+  };
 
   static contextType = AuthContext;
 
@@ -49,22 +50,47 @@ class Booking extends Component {
       });
   };
 
+  deleteBookingHandler = bookingId => {
+    this.setState({ isLoading: true });
+    const requestBody = {
+      query: `
+        mutation {
+          cancelBooking(bookingId: "${bookingId}") {
+            _id
+            title
+          }
+        }
+      `
+    };
+
+    const token = this.context.token;
+    const config = { headers: { Authorization: "bearer " + token } };
+
+    axios
+      .post("http://localhost:8000/graphql", requestBody, config)
+      .then(res => {
+        this.setState(prevState => {
+          const updatedBooking = prevState.bookings.filter(booking => {
+            return booking._id !== bookingId;
+          });
+          return { bookings: updatedBooking, isLoading: false };
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ isLoading: false });
+      });
+  };
+
   render() {
     return (
       <React.Fragment>
-      {this.state.isLoading ? (
-        <Spinner />
-      ) : (
-        <ul>
-          {this.state.bookings.map(booking => (
-            <li key={booking._id}>
-              {booking.event.title} -{' '}
-              {new Date(booking.createdAt).toLocaleDateString()}
-            </li>
-          ))}
-        </ul>
-      )}
-    </React.Fragment>
+        {this.state.isLoading ? (
+          <Spinner />
+        ) : (
+          <BookingList bookings={this.state.bookings} onDelete={this.deleteBookingHandler} />
+        )}
+      </React.Fragment>
     );
   }
 }
